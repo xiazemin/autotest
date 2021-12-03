6fddada37cee7fe3f4e9e6de2ba5ea966aa1078f
xiazemin <xiazemin@shimo.im>
2021-12-03 16:04:16 +0800

    change mock to comment

--- gotests/main.go
+++ gotests/main.go
@@ -51,7 +51,7 @@ import (
 )
 
 var (
-	onlyFuncs          = flag.String("only", "", `regexp. generate tests for functions and methods that match only. Takes precedence over -all`)
+	onlyFuncs          = flag.String("only", "", `regexp. generate tests for functions and methods that match:gotests -only ^XXX$ PATH; Takes precedence over -all`)
 	exclFuncs          = flag.String("excl", "", `regexp. generate tests for functions and methods that don't match. Takes precedence over -only, -exported, and -all`)
 	exportedFuncs      = flag.Bool("exported", false, `generate tests for exported functions and methods. Takes precedence over -only and -all`)
 	allFuncs           = flag.Bool("all", false, "generate tests for all functions and methods")
@@ -77,7 +77,7 @@ var (
 	named bool
 )
 
-const CurrentVersion = "v0.1.11"
+const CurrentVersion = "v0.1.12"
 
 func main() {
 	flag.Parse()
